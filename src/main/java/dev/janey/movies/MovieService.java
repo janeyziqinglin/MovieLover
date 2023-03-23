7509c895922aedb321a8f7fd45f314e4db53d9fd
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-03-23 12:17:33 -0400

    Implement the review part of the backend

--- src/main/java/dev/janey/movies/MovieService.java
+++ src/main/java/dev/janey/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.janey.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 /*
@@ -17,5 +19,9 @@ public class MovieService {
   private MovieRepository movieRepository;
   public List<Movie> allMovies(){
     return movieRepository.findAll();
+  }
+
+  public Optional<Movie> singleMovie(String imdbId){
+    return movieRepository.findMovieByImdbId(imdbId);
   }
 }
